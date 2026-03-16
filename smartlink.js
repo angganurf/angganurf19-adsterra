ea3940a18b72ed982e6aa9b39e783c999fa9e0a5
Angga Nurf <31128877+angganurf@users.noreply.github.com>
2026-03-17 02:43:54 +0700

    Create smartlink.js

---
+++ smartlink.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://www.effectivegatecpm.com/xzgn2xxb?key=0e6c0dec4a3a59580433be63cc2446b1',
+'https://www.effectivegatecpm.com/xzgn2xxb?key=0e6c0dec4a3a59580433be63cc2446b1',
+'https://www.effectivegatecpm.com/xzgn2xxb?key=0e6c0dec4a3a59580433be63cc2446b1'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
